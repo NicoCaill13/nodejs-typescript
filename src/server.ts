cbb78f3d7241e27eb7d3ba55faf9b562be9bb826
nicolas.cailleux <nicolas.cailleux@keepcool.fr>
2021-01-07 15:21:03 +0100

    serve

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`⚡️[server]⚡️: Server is running at http://localhost:${port}`));
+
+app.listen(port, () => {
+    console.log(`⚡️[server]⚡️: Server is running at http://localhost:${port}`);
+});
