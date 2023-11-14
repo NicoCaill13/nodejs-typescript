07f8b64134271ec3463790ef47519bf86135544b
NicoCaill13 <n.cailleux13@gmail.com>
2023-11-14 10:18:04 +0100

    test port

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+import axios from 'axios';
+import 'dotenv/config'
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', async (req, res) => {
+    try {
+        const result = await axios.get(`${process.env.APP_SILICO}/api/list/country`, {
+            headers: {
+                'apiKey': process.env.APP_API_KEY
+            }
+        });
+        console.log(result.data)
+        res.json(result.data)
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
 
 app.listen(port, () => {
     console.log(`⚡️[server]⚡️: Server is running at http://localhost:${port}`);
--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,28 @@
 import { expect } from 'chai';
 import 'mocha';
-import {Spartan} from '../src/Entities/Spartan'
+import axios from 'axios';
+import { Spartan } from '../src/Entities/Spartan'
+import 'dotenv/config'
 
 describe('First test', () => {
     it('should return a valid object', () => {
         const spartan = new Spartan('Leonidas', 1020);
         expect(spartan).to.be.a('object');
+    });
+});
+
+
+describe('Second test', () => {
+    it('should return a valid JSON', async () => {
+        try {
+            const result = await axios.get(`${process.env.APP_SILICO}/api/list/country`, {
+                headers: {
+                    'apiKey': process.env.APP_API_KEY
+                }
+            });
+            console.log(result.data)
+        } catch (error) {
+            console.log(error.message)
+        }
     });
 });
